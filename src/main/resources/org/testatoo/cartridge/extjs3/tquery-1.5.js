d5001d976bcd8baf785817de61f5d92b2177cd8a
David Avenante <altus34@gmail.com>
2011-03-05 19:48:51 -0500

    add licence header

--- src/main/resources/org/testatoo/cartridge/extjs3/tquery-1.5.js
+++ src/main/resources/org/testatoo/cartridge/extjs3/tquery-1.5.js
@@ -1,17 +1,17 @@
-/*!
- * jQuery JavaScript Library v1.5
- * http://jquery.com/
+/*
+ * Copyright (C) 2008 Ovea <dev@testatoo.org>
  *
- * Copyright 2011, John Resig
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://jquery.org/license
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- * Copyright 2011, The Dojo Foundation
- * Released under the MIT, BSD, and GPL Licenses.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Date: Mon Jan 31 08:31:29 2011 -0500
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 (function( window, undefined ) {
 
